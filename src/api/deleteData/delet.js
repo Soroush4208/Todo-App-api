d21ef74957111a21a2ac857a638705c570882b8c
soroush <soroush.r80@gmail.com>
2024-04-06 07:37:26 +0330

    feat:create function delete by api

--- src/api/deleteData/delet.js
+++ src/api/deleteData/delet.js
@@ -1,7 +1,8 @@
 import axios from "axios"
-import { BASE_URL } from "../const"
+import { BASE_URL } from "../const";
 
 export const deleteData = async(id)=>{
     const response = await axios.delete(`${BASE_URL}/orders/${id}`)
+    console.log(response);
     return response ;
 }
