3102bed854a4ba55b426ddbf10335adc8fbf4e2d
soroush <soroush.r80@gmail.com>
2024-04-09 00:42:02 +0330

    ref : refactor code

--- src/api/deleteData/delet.js
+++ src/api/deleteData/delet.js
@@ -4,5 +4,6 @@ import { BASE_URL } from "../const";
 export const deleteData = async(id)=>{
     const response = await axios.delete(`${BASE_URL}/orders/${id}`)
     console.log(response);
+    console.log(response);
     return response ;
 }
